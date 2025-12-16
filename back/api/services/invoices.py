3ec8f294b8cdd27237a599a894ce90ba25d8bab0
santorem <santorem@students.zhaw.ch>
2025-12-16 16:05:25 +0100

    invoices url

--- back/api/services/invoices.py
+++ back/api/services/invoices.py
@@ -13,7 +13,7 @@ def generate_invoice(report, request=None):
 
     html_content = generate_invoice_pdf(invoice)
 
-    base_url = settings.STATIC_ROOT
+    base_url = "file:///staticfiles/"
 
     pdf_file = HTML(string=html_content, base_url=base_url).write_pdf()
 
